2c81dc0d37c39f626741cccaa8ab10283e42d6ea
Giang <ngangiangtran91@gmail.com>
2021-10-05 11:06:42 +0700

    g

---
+++ story_content/user.js
@@ -0,0 +1,186 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5cyTZM1rWjO":
+        Script1();
+        break;
+      case "6qd057ebMSX":
+        Script2();
+        break;
+      case "5VyuIJyCS9i":
+        Script3();
+        break;
+      case "6Y7cQuIXMeT":
+        Script4();
+        break;
+      case "5xFWqPlcRqs":
+        Script5();
+        break;
+      case "5gip1HkloG1":
+        Script6();
+        break;
+      case "6eRjDVHxkfO":
+        Script7();
+        break;
+      case "6O1sJKAlIit":
+        Script8();
+        break;
+      case "5c8T93zQpak":
+        Script9();
+        break;
+      case "63qdivcD88Y":
+        Script10();
+        break;
+      case "5tUVttqrwV0":
+        Script11();
+        break;
+      case "6VB386Qo5U9":
+        Script12();
+        break;
+      case "5yCFudPs9E2":
+        Script13();
+        break;
+      case "5h68CZqiICl":
+        Script14();
+        break;
+      case "5kJBG3XHeOG":
+        Script15();
+        break;
+      case "6pj7Mj41oYv":
+        Script16();
+        break;
+      case "5c8HBczpiUE":
+        Script17();
+        break;
+      case "5wGY6jTZCrx":
+        Script18();
+        break;
+      case "5yQQeVvchPB":
+        Script19();
+        break;
+      case "6XdaXFPQ8je":
+        Script20();
+        break;
+      case "6SAjUHJzlBv":
+        Script21();
+        break;
+      case "6nBHC46FcZ4":
+        Script22();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script2()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script3()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script4()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script5()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script6()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script7()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script8()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script9()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script10()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script11()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script12()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script13()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script14()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script15()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script16()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script17()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script18()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script19()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script20()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script21()
+{
+  window.parent.parent.location.href = parent.parent.siteurl
+}
+
+function Script22()
+{
+  window.parent.parent.location.href = parent.parent.siteurl + "/chuyentiep.php?type=1&courseID=" + parent.parent.courseid+ "&scormID="+parent.parent.scoid + "&lessonID="+parent.parent.lessonid + "&ID="+parent.parent.id
+
+
+
+}
+
